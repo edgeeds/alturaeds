0847345f27c82807c2955a55ccb07a52af056b7b
Sherest Mehta <shmehta@adobe.com>
2024-03-04 10:24:09 +0530

    updated new cfrenderer block

--- blocks/cfrenderer/cfrenderer.js
+++ blocks/cfrenderer/cfrenderer.js
@@ -1,7 +1,45 @@
-export default function decorate(block) {
-    /*const cols = [...block.firstElementChild.children];
-    block.classList.add(`columns-${cols.length}-cols`);*/
+function sanitiseHTML(htmlString) {
+  if (!htmlString) return null;
 
-    const query = block.querySelector('a[href]');
-    console.log("Sherest >>", query);
+  // Initialize a new DOMParser instance
+  const parser = new DOMParser();
+
+  // Parse the HTML string
+  const doc = parser.parseFromString(htmlString, 'text/html');
+
+  // Remove any <script> tags to mitigate potential XSS attacks
+  Array.from(doc.getElementsByTagName('script')).forEach((script) => script.remove());
+
+  return doc.body.childNodes;
+}
+async function getContent(sURL) {
+  if (!sURL) return null;
+  let result;
+
+  try {
+    const url = new URL(sURL);
+    const res = await fetch(url);
+    const json = await res.json();
+    result = sanitiseHTML(json?.data?.textBlockCfByPath?.item?.body?.html);
+  } catch (error) {
+    console.error('error occurred > ', error);
+  }
+
+  return result || 'No text';
+}
+export default async function decorate(block) {
+  const queryElement = block.querySelector('a[href]');
+  if (queryElement) {
+    const queryURL = queryElement.href;
+    const parentDiv = document.createElement('div');
+    parentDiv.classList.add('cfrenderer-block');
+    const nodeList = await getContent(queryURL);
+
+    nodeList.forEach((node) => {
+      parentDiv.appendChild(node.cloneNode(true));
+    });
+
+    // replace the default block with the above
+    queryElement.replaceWith(parentDiv);
+  }
 }
